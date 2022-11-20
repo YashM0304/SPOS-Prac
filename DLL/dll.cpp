576316710fee39989287c614884a506ec3291700
YashM0304 <111646744+YashM0304@users.noreply.github.com>
2022-11-20 13:32:55 +0530

    Add files via upload

---
+++ DLL/dll.cpp
@@ -0,0 +1,13 @@
+using System
+using Cals
+Public Static Program
+{
+Publlic Static Main(args As String())
+{
+Console.WriteLine(Function.Add(5,20))
+Console.WriteLine(Function.Sub(30,20))
+Console.WriteLine(Function.Mult(5,5))
+Console.WriteLine(Function.Div(40,20))
+Console.ReadKey()
+}
+}
